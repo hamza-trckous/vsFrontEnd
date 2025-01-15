a25fa0fe7fb6cbe26b8e6f5d715f43f63da3da44
unknown <hamzatricks@gmail.com>
2025-01-15 12:46:34 +0100

    after final icon6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,18 +3,25 @@ import "../styles/globals.css";
 import Nav from "../components/Nav";
 import TopNavBar from "@/components/topNavBar";
 import { AuthProvider } from "@/context/AuthContext";
-import MainContent from "@/components/MainContent"; // Import the MainContent component
+import MainContent from "@/components/MainContent";
 import { UserProvider } from "@/context/UserContext";
 import { ShippingProvider } from "@/context/ShippingContext";
 import AlertProvider from "@/context/useAlert";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Babybloom",
+  icons: {
+    icon: "/favicon.png",
+    // You can also add these for better compatibility
+    shortcut: "/favicon.png",
+    apple: "/favicon.png",
+  },
+};
 
 const RootLayout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.png" type="image/png" />
-        <title>Babybloom</title>
-      </head>
       <body className="antialiased">
         <AlertProvider>
           <ShippingProvider>
