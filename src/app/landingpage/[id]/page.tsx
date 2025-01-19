a7cea8b04fd94b4d3fa28293c9d62c2e5daf778a
unknown <hamzatricks@gmail.com>
2025-01-19 21:45:35 +0100

    after final officiel comeback

--- src/app/landingpage/[id]/page.tsx
+++ src/app/landingpage/[id]/page.tsx
@@ -3,7 +3,9 @@ import LandingPageId from "@/components/LandingPageID";
 import React from "react";
 import { useParams } from "next/navigation";
 const Page = () => {
-  const { id } = useParams<{ id: string }>();
+  const params = useParams<{ id: string }>();
+  const id = params?.id;
+  if (!id) return null;
   return <LandingPageId productId={id} />;
 };
 
